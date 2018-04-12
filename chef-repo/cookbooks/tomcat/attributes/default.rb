6e3c9fb2f8c0f56ff8f2ba070eb7c838563369eb
Raghu K <raghuk.vit@gmail.com>
2018-04-12 01:29:59 +0000

    Updating CHEF

--- chef-repo/cookbooks/tomcat/attributes/default.rb
+++ chef-repo/cookbooks/tomcat/attributes/default.rb
@@ -1,1 +1,7 @@
 default['tomcat']['URL']='http://redrockdigimark.com/apachemirror/tomcat/tomcat-9/v9.0.7/bin/apache-tomcat-9.0.7.tar.gz'
+default['tomcat']['TAR_FILE']="#{default['tomcat']['URL']}".split('/').last
+default['tomcat']['TAR_DIR']="#{default['tomcat']['TAR_FILE']}".gsub('.tar.gz','')
+default['tomcat']['LOC']="/opt/#{default['tomcat']['TAR_DIR']}"
+default['tomcat']['WAR']='https://github.com/cit-ager/APP-STACK/raw/master/student.war'
+default['tomcat']['JDBC_JAR']='https://github.com/cit-ager/APP-STACK/raw/master/mysql-connector-java-5.1.40.jar'
+## 
--- chef-repo/cookbooks/tomcat/recipes/install.rb
+++ chef-repo/cookbooks/tomcat/recipes/install.rb
@@ -1,4 +1,37 @@
+
 remote_file 'Downloading Tomcat' do 
-    path '/tmp/tomcat.tgz'
+    path "/opt/#{node['tomcat']['TAR_FILE']}"
     source "#{node['tomcat']['URL']}"
+end
+
+execute 'Extracting Tomcat TAR' do
+    command "tar -xf /opt/#{node['tomcat']['TAR_FILE']}"
+    cwd '/opt'
+    not_if "ls -ld #{node['tomcat']['LOC']}"
+end
+
+Dir["#{node['tomcat']['LOC']}/webapps/*"].each do |loc|
+    if File.file?("#{loc}")
+        file "Removing File #{loc}" do
+            path "#{loc}"
+            action :delete
+        end
+    elsif File.directory?("#{loc}") 
+        directory "Removing Directory #{loc}" do
+            path "#{loc}"
+            recursive true
+            action :delete
+        end
+    end
+end 
+
+remote_file 'Downloading Student War file' do 
+    path "/opt/#{node['tomcat']['TAR_FILE']}/webapps/student.war"
+    source "#{node['tomcat']['WAR']}"
+end
+
+remote_file 'Downloading JDBC jar file' do 
+    path "/opt/#{node['tomcat']['TAR_FILE']}/lib/mysql-connector-java-5.1.40.jar"
+    source "#{node['tomcat']['WAR']}"
+    not_if "ls -l /opt/#{node['tomcat']['TAR_FILE']}/lib/mysql-connector-java-5.1.40.jar"
 end
